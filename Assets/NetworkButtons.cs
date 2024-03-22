77f1c2b72de11ce8e7cc6facff8728e9ecb098b2
MagicalMagee <132414527+MagicalMagee@users.noreply.github.com>
2024-03-21 22:13:21 -0700

    Update NetworkButtons.cs

--- Assets/NetworkButtons.cs
+++ Assets/NetworkButtons.cs
@@ -4,10 +4,13 @@ using Unity.Netcode;
 using Unity.Netcode.Transports.UTP;
 using UnityEngine;
 
+// Reference: https://www.youtube.com/watch?v=stJ4SESQwJQ
+// For the sake of easier testing and playablity
 public class NetworkButtons : MonoBehaviour {
     private void OnGUI() {
         GUILayout.BeginArea(new Rect(10, 10, 300, 300));
         if (!NetworkManager.Singleton.IsClient && !NetworkManager.Singleton.IsServer) {
+            // Puts Buttons on Top left of Screen
             if (GUILayout.Button("Host")) NetworkManager.Singleton.StartHost();
             if (GUILayout.Button("Server")) NetworkManager.Singleton.StartServer();
             if (GUILayout.Button("Client")) NetworkManager.Singleton.StartClient();
